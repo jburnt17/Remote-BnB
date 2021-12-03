07cf335f99550ed9d1854f13d80b7e15563fcd39
Jared B <jburnt17@gmail.com>
2021-12-03 17:52:14 -0500

    added api route folder

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+router.use("/api", apiRouter);
+
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
 });
 
 module.exports = router;
