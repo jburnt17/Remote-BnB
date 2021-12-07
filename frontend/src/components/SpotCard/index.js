379e34aa63a0616acdfcdd18d2cd7adb7f193e3d
Jared B <jburnt17@gmail.com>
2021-12-07 18:22:05 -0500

    added modal func

--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -8,15 +8,20 @@ import "./styles.css";
 function SpotCard({ spotId, name, city, state, price }) {
   const dispatch = useDispatch();
   const [optionsShow, setOptionsShow] = useState(true);
-
+  
   useEffect(() => {
-    const options = document.querySelector('.spot-options');
-    if(!optionsShow) {
-      options.style.display = 'flex';
+    const option = document.querySelector(`#option-${spotId}`);
+    option.style.zIndex = '99';
+    if (!optionsShow) {
+      option.style.display = "flex";
     } else {
-      options.style.display = 'none';
+      option.style.display = "none";
     }
   }, [optionsShow]);
+
+  const upper = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   return (
     <div className="spot-card">
@@ -25,16 +30,24 @@ function SpotCard({ spotId, name, city, state, price }) {
           className="spot-image"
           src="https://a0.muscache.com/im/pictures/6d0d3c2b-8515-40fd-9039-c36ea1c067db.jpg?im_w=720"
         />
-        <DotsCircleHorizontalIcon onClick={() => setOptionsShow(!optionsShow)} className="spot-dots" />
-        <ol className="spot-options">
-          <button className="options-link delete-button" onClick={() => dispatch(deleteSpot(spotId))}>Delete</button>
-          <NavLink to={`/api/spots/${spotId}/edit`} className="options-link" >
+        <DotsCircleHorizontalIcon
+          onClick={() => setOptionsShow(!optionsShow)}
+          className="spot-dots"
+        />
+        <ol id={`option-${spotId}`} className="spot-options">
+          <button
+            className="options-link delete-button"
+            onClick={() => dispatch(deleteSpot(spotId))}
+          >
+            Delete
+          </button>
+          <NavLink to={`/api/spots/${spotId}/edit`} className="options-link">
             Edit
           </NavLink>
         </ol>
       </div>
       <div className="spot-desc">
-        {name} | {city}, {state}
+        {upper(city)}, {upper(state)}
       </div>
       <div className="spot-price">${price} / night</div>
     </div>
