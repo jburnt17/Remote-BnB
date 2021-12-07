89f6b0ba3cf194ae0d48f2de8624dceb20715a55
Jared B <jburnt17@gmail.com>
2021-12-07 01:33:42 -0500

    added spot creation

---
+++ backend/routes/api/host.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const asyncHandler = require("express-async-handler");
+const {Spot} = require("../../db/models");
+
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const spot = await Spot.create(req.body);
+    res.json(spot);
+  })
+);
+
+
+module.exports = router;
