5b739ad711901cdbec831c9c3ae9843cf1518aaf
Jared B <jburnt17@gmail.com>
2021-12-07 19:19:26 -0500

    sadded edit spot form

--- frontend/src/components/EditSpot/index.js
+++ frontend/src/components/EditSpot/index.js
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import NavBar from "../NavBar";
 
 function EditSpot() {
   const { spotId } = useParams();
+  const spots = useSelector((state) => state.spotState.entries);
+  const { city, state, country, address, name, price } = spots[spotId];
+
+  const [editCity, setEditCity] = useState(city);
+  const [editState, setEditState] = useState(state);
+  const [editCountry, setEditCountry] = useState(country);
+  const [editAddress, setEditAddress] = useState(address);
+  const [editName, setEditName] = useState(name);
+  const [editPrice, setEditPrice] = useState(price);
+
+
+  const updateCity = (e) => setEditCity(e.target.value);
+  const updateState = (e) => setEditState(e.target.value);
+  const updateCountry = (e) => setEditCountry(e.target.value);
+  const updateAddress = (e) => setEditAddress(e.target.value);
+  const updateName = (e) => setEditName(e.target.value);
+  const updatePrice = (e) => setEditPrice(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
   return (
-  <div>Hello, {spotId}</div>
+    <div>
+      <NavBar />
+      <div>Hello, {spotId}</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={updateAddress}
+          value={address}
+          placeholder="Address"
+          name="address"
+        />
+        <input
+          type="text"
+          onChange={updateCity}
+          value={city}
+          placeholder="City"
+          name="city"
+        />
+        <input
+          type="text"
+          onChange={updateState}
+          value={state}
+          placeholder="State"
+          name="state"
+        />
+        <input
+          type="text"
+          onChange={updateCountry}
+          value={country}
+          placeholder="Country"
+          name="country"
+        />
+        <input
+          type="text"
+          onChange={updateName}
+          value={name}
+          placeholder="Title"
+          name="title"
+        />
+        <input
+          type="number"
+          onChange={updatePrice}
+          value={price}
+          placeholder="Price"
+          name="price"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,7 +1,7 @@
-// frontend/src/components/SignupFormPage/index.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import NavBar from "../NavBar";
 import * as sessionActions from "../../store/session";
 
 function SignupFormPage() {
@@ -32,50 +32,53 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div>
+      <NavBar />
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Email
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Confirm Password
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 }
 
